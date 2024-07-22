173df81a891977c80f9f6cff2cb568141670c7f2
ghinzuka <baptiste.pouquerou@gmail.com>
2024-07-22 17:26:37 +0200

    coding using two const to get the result from two promises, retrive the result (fulfilled or rejected) in results const, we use await to asynchrone the function, the we map the result in key value items, finnally for each results, we check the status and return if filfilled or no

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,23 +1,21 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-const handleProfileSignup = async (
-  firstName,
-  lastName,
-  fileName,
-) => {
-  const result = [];
-  try {
-    const user = await signUpUser(firstName, lastName);
-    result.push({ status: 'fulfilled', value: user });
-    await uploadPhoto(fileName);
-  } catch (error) {
-    result.push({
-      status: 'rejected',
-      value: error.toString(),
-    });
-  }
-  return result;
-};
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+  const results = await Promise.allSettled([userPromise, photoPromise]);
 
-export default handleProfileSignup;
+  return results.map((result) => {
+    if (result.status === 'fulfilled') {
+      return {
+        status: 'fulfilled',
+        value: result.value,
+      };
+    }
+    return {
+      status: 'rejected',
+      value: result.reason,
+    };
+  });
+}
