171226578b7784b87f5a366973be16f7824e2633
ghinzuka <baptiste.pouquerou@gmail.com>
2024-08-19 10:33:40 +0200

    using the async for to iterate trhough async_generator function asynchronously

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,10 +3,10 @@
 takes no arguments."""
 import asyncio
 import random
-from typing import Generator
+from typing import AsyncGenerator
 
 
-async def async_generator() -> Generator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """The coroutine will loop 10 times, each time
     asynchronously wait 1 second"""
     for i in range(10):
