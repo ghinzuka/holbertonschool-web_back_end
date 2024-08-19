c665328ece2bcc4eb40980d2a7e73dd915777e5a
ghinzuka <baptiste.pouquerou@gmail.com>
2024-08-19 10:03:40 +0200

    using yield for the first time instead of return

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,6 +6,7 @@ from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
+    """The coroutine will loop 10 times, each time asynchronously wait 1 second"""
     for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
