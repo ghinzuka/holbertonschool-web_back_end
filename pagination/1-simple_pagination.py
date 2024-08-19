a90400094611e0b03f33a7517f0e68f85946d417
ghinzuka <baptiste.pouquerou@gmail.com>
2024-08-19 14:22:30 +0200

    using the helper index_range to return only a list of csv information from index page to the last index while securing the type and the value of the page we pass in the class

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""mplement a method named get_page that takes two integer arguments
+page with default value 1 and page_size with default value 10."""
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """return a tuple of size two containing a start index
+    and an end index corresponding to the range of indexes
+    to return in a list for those
+    particular pagination parameters."""
+    start_index = ((page - 1) * page_size)
+    end_index = page * page_size
+    return start_index, end_index
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """return the appropriate page of the dataset
+        (i.e. the correct list of rows)"""
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        start_index, end_index = index_range(page, page_size)
+        dataset = self.dataset()
+        if len(dataset) <= start_index or len(dataset) <= end_index:
+            return []
+        return dataset[start_index:end_index]
